c9471c1de331b881f0a156bc743b8c9ec6f06580
Jeroen Voogd <jpvoogd@icloud.com>
2023-08-05 10:40:10 +0200

    Removed some debug code.

--- script.js
+++ script.js
@@ -23,11 +23,3 @@ const navSlide = () => {
 };
 
 navSlide();
-
-var docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(document.querySelectorAll("*"), function (el) {
-  if (el.offsetWidth > docWidth) {
-    console.log(el);
-  }
-});
